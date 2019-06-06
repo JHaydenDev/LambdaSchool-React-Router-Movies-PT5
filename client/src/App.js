0d69dd0865dc45cb76603e1f771c96ae753a0690
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2019-06-05 21:46:45 -0500

    Got the card to render.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import SavedList from './Movies/SavedList';
-import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie';
-import { Route } from 'react-router-dom';
+import SavedList from "./Movies/SavedList";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
+import { Route } from "react-router-dom";
 
 export default class App extends Component {
   constructor() {
@@ -24,7 +24,12 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
         <Route exact path="/" component={MovieList} />
-        <Route path="/movies:id" component={Movie} />
+        <Route
+          path="/movies/:id"
+          render={props => {
+            return <Movie {...props} addToSavedList={this.addToSavedList} />;
+          }}
+        />
       </div>
     );
   }
